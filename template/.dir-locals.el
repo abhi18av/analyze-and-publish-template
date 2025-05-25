baeeb5d73e2d9f63b448f069484a71329f0e3e34
Abhinav Sharma <abhi18av@gmail.com>
2025-05-25 13:54:29 +0200

    add a project specific .emacs folder for elisp

--- template/.dir-locals.el
+++ template/.dir-locals.el
@@ -1,3 +1,3 @@
-(((nil . ((eval . (load-file (expand-file-name "project-setup.el" (projectile-project-root)))))))
+(((nil . ((eval . (load-file (expand-file-name ".emacs.proj/project-setup.el" (projectile-project-root)))))))
  (clojure-mode . ((cider-preferred-build-tool . clojure-cli)
                   (cider-clojure-cli-parameters . "-A:nextjournal/clerk"))))
