271f72be113d690b987fca6ae0f430c1199ef4eb
Abhinav Sharma <abhi18av@gmail.com>
2025-05-25 13:49:26 +0200

    automatically load project automations

--- template/.dir-locals.el
+++ template/.dir-locals.el
@@ -1,2 +1,3 @@
-((clojure-mode . ((cider-preferred-build-tool . clojure-cli)
+(((nil . ((eval . (load-file (expand-file-name "project-setup.el" (projectile-project-root)))))))
+ (clojure-mode . ((cider-preferred-build-tool . clojure-cli)
                   (cider-clojure-cli-parameters . "-A:nextjournal/clerk"))))
