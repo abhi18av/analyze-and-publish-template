30e8fa13247058493889f73a15bdf62bfd93e35c
Abhinav Sharma <abhi18av@outlook.com>
2025-05-29 23:27:52 +0200

    bundle all dotfiles and dotfolders

--- template/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-(((nil . ((eval . (load-file (expand-file-name ".emacs.proj/project-setup.el" (projectile-project-root)))))))
- (clojure-mode . ((cider-preferred-build-tool . clojure-cli)
-                  (cider-clojure-cli-parameters . "-A:nextjournal/clerk"))))
--- template/.fish/aliases.sh
+++
@@ -1,2 +0,0 @@
-alias gs 'git status'
-alias gp 'git pull'
